4633b1fda8752f6ae8b63ff9fadf78716b7f5ab8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-10-18 12:39:33 +0900

    chore: fix stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -13,5 +13,9 @@ module.exports = {
     // 'order/properties-order' is disabled in 'stylelint-config-xo' for now.
     // See https://github.com/sindresorhus/stylelint-config-xo/pull/2#issuecomment-363438756.
     'order/properties-order': propertiesOrder,
+    // Use `scss/at-rule-no-unknown` instead of `at-rule-no-unknown` for SCSS.
+    // See https://github.com/kristerkari/stylelint-scss/blob/HEAD/src/rules/at-rule-no-unknown/README.md.
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
 };
