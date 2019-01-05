4ab28705fcbac4e55603e67deab19ad0540a8aa8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-01-05 11:14:29 +0900

    fix: add TODO

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-alert */
 
+// TODO: use `onServiceWorkerUpdateReady` instead: https://github.com/gatsbyjs/gatsby/pull/10432
+
 exports.onServiceWorkerUpdateFound = () => {
   const answer = window.confirm(
     'This application has been updated. Reload to display the latest version?',
