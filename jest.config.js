48e3d4efd5345acf448e7c59152ad50c8d6971f7
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-01-07 21:00:32 +0900

    feat: introduce @storybook/addon-storyshots

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  globals: {
+    __PATH_PREFIX__: '',
+  },
+  testURL: 'http://localhost',
+  setupFiles: ['<rootDir>/.jest/loader-shim.js', '<rootDir>/.jest/register-context.js'],
+};
