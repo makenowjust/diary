305aa4ee702c28cfc50cd5874cab612eb17f0f74
TSUYUSATO Kitsune (from Travis CI) <make.just.on@gmail.com>
2019-03-31 06:56:31 +0000

    Deploy MakeNowJust/diary to github.com/MakeNowJust/diary.git:release

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ab0de58337e9cba9a81d.js"
+    "url": "webpack-runtime-7ae46ef0dc241423edbd.js"
   },
   {
     "url": "app-3f6e4e55fd0b1aca1c55.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f16e4db3dfda73f3475ec02669b71a85"
+    "revision": "68a16f60970c69d4d67dc5ab53b23e6d"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
