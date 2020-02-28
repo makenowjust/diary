9c728669b5f68013384d397f41d29234afc45e71
MakeNowJust <MakeNowJust@users.noreply.github.com>
2020-02-28 06:50:48 +0000

    Automated deployment: Fri Feb 28 06:50:48 UTC 2020 c8b4f698ce5b8252d6e23e46a0cd4a82079ec4a3

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-44cfc3cb369276f51f4f.js"
   },
   {
-    "url": "commons-150117b40da8983b5b0b.js"
+    "url": "commons-5e70a65ec0efa6ed740a.js"
   },
   {
     "url": "app-b07110e0996375a2212a.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "487977a9714ead692c2c59b7770a5b34"
+    "revision": "ac8b4da90bb4db0555cacb0ed9f8f58d"
   },
   {
     "url": "manifest.webmanifest",
