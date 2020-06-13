e24a6024a5cf73d963aa6d552e6248ec4052d7cf
MakeNowJust <MakeNowJust@users.noreply.github.com>
2020-06-13 05:16:52 +0000

    Automated deployment: Sat Jun 13 05:16:52 UTC 2020 a1f2896ed70a3ef8a5016ce51455f572e3467cc4

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-526e1e9cb5acf79d2d0d.js"
+    "url": "webpack-runtime-c0c859443e6fba26f6f6.js"
   },
   {
     "url": "framework-059e462a143188f8932f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bc9cc040b0f59edf0ac8a2d9a99d246f"
+    "revision": "7e7aae3e412791482d8ae35d1f166751"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ea4f48d17fd2572edfd4.js"
