b83fa20938f1e5b50a600dfd934a33b42d7252b3
MakeNowJust <MakeNowJust@users.noreply.github.com>
2020-07-29 15:57:59 +0000

    Automated deployment: Wed Jul 29 15:57:59 UTC 2020 0a11e295ce39c4702c1b0234b485814a66f1e109

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-6b64f7a8b8426bf49929.js"
   },
   {
-    "url": "app-88cc98c17d483933f68a.js"
+    "url": "app-503cf4b371b33a8540cc.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "383ed53e1e241632b32906efd3ed507b"
+    "revision": "3dd66cfceec9fe53d41492ad78ceb3bc"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5c4f9d2cd7da9fef61f1.js"
@@ -140,7 +140,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-88cc98c17d483933f68a.js`))) {
+  if (!resources || !(await caches.match(`/app-503cf4b371b33a8540cc.js`))) {
     return await fetch(event.request)
   }
 
