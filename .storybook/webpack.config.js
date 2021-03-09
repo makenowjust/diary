176a9c95d349ed0402d70fe7269c3aea7bdd2e13
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-03-09 22:40:45 +0900

    chore: update snapshot

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,9 +4,6 @@ module.exports = (baseConfig, env, defaultConfig) => {
   // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
   defaultConfig.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
 
-  // Use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7).
-  defaultConfig.module.rules[0].use[0].loader = require.resolve('babel-loader');
-
   // Use @babel/preset-react for JSX and env (instead of staged presets).
   defaultConfig.module.rules[0].use[0].options.presets = [
     require.resolve('@babel/preset-react'),
